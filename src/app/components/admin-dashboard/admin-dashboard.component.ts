53f14785c9c8f0b22c35d4cec8762e12904435b6
Himanshu Koshti <Himfeedback74@gmail.com>
2020-11-21 12:07:33 +0530

    Front endtokenrole based redirection

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -19,6 +19,16 @@ export class AdminDashboardComponent implements OnInit {
       this.router.navigateByUrl('login')
       console.log("Login First")
     }
+      const jwt = JSON.stringify(sessionStorage.getItem("token"));
+      let jwtData = jwt.split('.')[1]
+      let decodedJwtJsonData = window.atob(jwtData)
+      let decodedJwtData = JSON.parse(decodedJwtJsonData)
+      if(decodedJwtData.authorities == "ROLE_ADMIN")
+        this.router.navigateByUrl('Admin')
+        else if (decodedJwtData.authorities == "ROLE_EMPLOYEE")
+          this.router.navigateByUrl('employeeDashboard')
+        else 
+          this.router.navigateByUrl('CustomerHome')
   }
   
 
--- src/app/components/customer/chome/chome.component.ts
+++ src/app/components/customer/chome/chome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chome',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(sessionStorage.getItem("token") === null){
+      this.router.navigateByUrl('login')
+      console.log("Login First")
+    }
+    const jwt = JSON.stringify(sessionStorage.getItem("token"));
+      let jwtData = jwt.split('.')[1]
+      let decodedJwtJsonData = window.atob(jwtData)
+      let decodedJwtData = JSON.parse(decodedJwtJsonData)
+      if(decodedJwtData.authorities == "ROLE_ADMIN")
+        this.router.navigateByUrl('Admin')
+        else if (decodedJwtData.authorities == "ROLE_EMPLOYEE")
+          this.router.navigateByUrl('employeeDashboard')
+        else 
+          this.router.navigateByUrl('CustomerHome')
   }
 
 }
