27043f7b1b9fb4292afa678e6c1314e843ff5d75
rkrohitkakade <kakade2506@gmail.com>
2020-11-26 22:47:51 +0530

    Customer Account Details for Employee

--- src/app/components/Employee/customer/customer-account-details/customer-account-details.component.ts
+++ src/app/components/Employee/customer/customer-account-details/customer-account-details.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-customer-account-details',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-account-details.component.css']
 })
 export class CustomerAccountDetailsComponent implements OnInit {
+  AllCustomers: any;
 
-  constructor() { }
+  constructor(private _service:MainService,private _http:HttpClient) { }
 
   ngOnInit(): void {
+    this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
   }
 
 }
