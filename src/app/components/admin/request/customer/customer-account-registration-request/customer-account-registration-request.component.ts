3afd5c7d34f443fbdf5a87f15a8b79456b0b3d57
rkrohitkakade <kakade2506@gmail.com>
2020-12-04 13:39:16 +0530

    Approve Reject for Account creation Request Admin

--- src/app/components/admin/request/customer/customer-account-registration-request/customer-account-registration-request.component.ts
+++ src/app/components/admin/request/customer/customer-account-registration-request/customer-account-registration-request.component.ts
@@ -19,17 +19,46 @@ export class CustomerAccountRegistrationRequestComponent implements OnInit {
   ngOnInit(): void {
     this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
   }
-  verifyAccount(customertemp){
-    customertemp.accountregstatus = 2;
-    customertemp.accounttype = customertemp.accountTypeId;
-    this._service.verifyAccountRequestApprovedReject(customertemp);
-    this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
-  }
-  rejectRegistrationRequest(customertemp){
-    customertemp.accountregstatus = 3;
-    customertemp.accounttype = customertemp.accountTypeId;
-    this._service.verifyAccountRequestApprovedReject(customertemp);
-    this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
-  }
+  // verifyAccount(customertemp:AccountCreationApproveRejectRequest){
+  //   customertemp.accountregstatus = 2;
+  //   customertemp.accounttype = customertemp.accounttype;
+  //   this._service.verifyAccountRequestApprovedReject(customertemp);
+  //   this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
+  // }
+
+  verifyAccount(customerResponse:AccountCreationApproveRejectRequest) //Created By Rohit
+    {
+      customerResponse.accountregstatus = 2;
+      this._service.verifyAccountRequestApprovedReject(customerResponse).subscribe(
+        data=>{
+          this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
+          console.log("User Approved");
+        },
+        error=>{
+          console.log("User not approved");
+        }
+      )
+    }
+
+  // rejectRegistrationRequest(customertemp){
+  //   customertemp.accountregstatus = 3;
+  //   customertemp.accounttype = customertemp.accounttype;
+  //   this._service.verifyAccountRequestApprovedReject(customertemp);
+  //   this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
+  // }
+
+  rejectRegistrationRequest(customerResponse:AccountCreationApproveRejectRequest) //Created By Rohit
+    {
+      customerResponse.accountregstatus = 3;
+      this._service.verifyAccountRequestApprovedReject(customerResponse).subscribe(
+        data=>{
+          this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
+          console.log("User rejected");
+        },
+        error=>{
+          console.log("User not rejected");
+        }
+      )
+    }
 
 }
