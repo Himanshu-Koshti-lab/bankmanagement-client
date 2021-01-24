2aa7a834867349b8b38982b95558d00f4e450831
Himanshu Koshti <Himfeedback74@gmail.com>
2021-01-24 12:31:00 +0530

    Customer account  and profile tab changes

--- src/app/components/Employee/customer/customer-profile-details/customer-profile-details.component.ts
+++ src/app/components/Employee/customer/customer-profile-details/customer-profile-details.component.ts
@@ -6,28 +6,16 @@ import { MainService } from 'src/app/services/main.service';
 @Component({
   selector: 'app-customer-profile-details',
   templateUrl: './customer-profile-details.component.html',
-  styleUrls: ['./customer-profile-details.component.css']
+  styleUrls: ['./customer-profile-details.component.css'],
 })
 export class CustomerProfileDetailsComponent implements OnInit {
-
   AllCustomers;
 
-  constructor(private _service:MainService,private _http:HttpClient) { }
+  constructor(private _service: MainService, private _http: HttpClient) {}
 
   ngOnInit(): void {
-    this._service.getCustomerForEmployeeFromRemote().subscribe((data) => this.AllCustomers = data);
+    this._service
+      .getCustomerForEmployeeFromRemote()
+      .subscribe((data) => (this.AllCustomers = data));
   }
-
-  deleteCustomer(customerResponse:CustomerResponse)
-    {
-      this._service.approveRegistrationRequest(customerResponse).subscribe(
-        data=>{
-          this._service.getCustomerFromRemote().subscribe((data) => this.AllCustomers = data);
-          console.log("User Approved");
-        },
-        error=>{
-          console.log("User not approved");
-        }
-      )
-    }
 }
--- src/app/components/Employee/employee-change-password/employee-change-password.component.ts
+++ src/app/components/Employee/employee-change-password/employee-change-password.component.ts
@@ -1,35 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
-import {FormBuilder, FormControl, FormGroup, NgForm, Validators}from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { ChangePassword } from 'src/app/model/change-password';
 
 @Component({
   selector: 'app-employee-change-password',
   templateUrl: './employee-change-password.component.html',
-  styleUrls: ['./employee-change-password.component.css']
+  styleUrls: ['./employee-change-password.component.css'],
 })
 export class EmployeeChangePasswordComponent implements OnInit {
-
   changePasswordForm: FormGroup;
-  Customer:any;
+  Customer: any;
   message: any;
 
-
-  constructor(private  _service:MainService) { }
+  constructor(private _service: MainService) {}
 
   ngOnInit(): void {
     this.changePasswordForm = new FormGroup({
-      'currentPassword': new FormControl(null,[Validators.required]),
-      'newPassword':new FormControl(null,[Validators.required])
-      });
+      currentPassword: new FormControl(null, [Validators.required]),
+      newPassword: new FormControl(null, [Validators.required]),
+    });
 
-
-    this._service.getEmployee().subscribe((data) => this.Customer = data);
+    this._service.getEmployee().subscribe((data) => (this.Customer = data));
   }
 
-  public changePassword(changePassword:ChangePassword){
-    let resp= this._service.changePasswordService(changePassword);
-   resp.subscribe((data)=>this.message=data);
-     }
-
+  public changePassword(changePassword: ChangePassword) {
+    let resp = this._service.changePasswordService(changePassword);
+    resp.subscribe((data) => (this.message = data));
+  }
 }
