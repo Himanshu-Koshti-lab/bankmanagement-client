8fd893ef52c5064a3083b9c9e168cb11bc5a27ef
Himanshu Koshti <Himfeedback74@gmail.com>
2021-01-26 14:47:29 +0530

    Fund Transfer Select option Added

--- src/app/components/customer/cfund-transfer/cfund-transfer.component.ts
+++ src/app/components/customer/cfund-transfer/cfund-transfer.component.ts
@@ -1,36 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, NgForm, Validators}from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { from } from 'rxjs';
-import{TransferFundRequest} from "src/app/model/transfer-fund-request"
+import { TransferFundRequest } from 'src/app/model/transfer-fund-request';
 import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-cfund-transfer',
   templateUrl: './cfund-transfer.component.html',
-  styleUrls: ['./cfund-transfer.component.css']
+  styleUrls: ['./cfund-transfer.component.css'],
 })
 export class CFundTransferComponent implements OnInit {
-
   transferFundForm: FormGroup;
- 
-  // transferfund: TransferFundRequest ; //added this
-  message:any;
- 
-  constructor(private  service:MainService) { }
- 
-  ngOnInit(): void {
-    this.transferFundForm = new FormGroup({
-      'senderAccountNumber':new FormControl(null,[Validators.required,Validators.minLength(6),Validators.maxLength(6),Validators.pattern("^[0-9]*$")]),
-      'receiverAccountNumber': new FormControl(null,[Validators.required,Validators.minLength(6),Validators.maxLength(6),Validators.pattern("^[0-9]*$")]),
-      'amount':new FormControl(null,[Validators.required,Validators.minLength(1),Validators.maxLength(5),Validators.pattern("^[0-9]*$")])
-      });
-  }
-    
-  public Transfernow(TransferFundRequest:TransferFundRequest){
-    let resp= this.service.doTransfer(TransferFundRequest);
-   resp.subscribe((data)=>this.message=data);
-  console.log(TransferFundRequest)
-     
-     }
 
+  // transferfund: TransferFundRequest ; //added this
+  AllAccount: any;
+  message: any;
+
+  constructor(private service: MainService) {}
+
+  ngOnInit(): void {
+    this.service
+      .getUserAccounts()
+      .subscribe((data) => (this.AllAccount = data));
+    this.transferFundForm = new FormGroup({
+      senderAccountNumber: new FormControl(null),
+      receiverAccountNumber: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(6),
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      amount: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(5),
+        Validators.pattern('^[0-9]*$'),
+      ]),
+    });
+  }
+  accountNumber: number = 0;
+  selectChangeHandler(event: any) {
+    this.accountNumber = event.target.value;
+    console.log(this.accountNumber);
+  }
+
+  public Transfernow(TransferFundRequest: TransferFundRequest) {
+    let resp = this.service.doTransfer(TransferFundRequest);
+    resp.subscribe((data) => (this.message = data));
+    console.log(TransferFundRequest);
+  }
+  checkAccount(accountNumber: number) {
+    if (accountNumber == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,16 +7,16 @@ export const environment = {
   clientId: 'ABCD',
   clientSecret: 'ABCD',
   scope: 'read',
-  USER_API: 'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8081',
-  TRANSACTION_API:
-    'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8082',
-  DASHBOARD_API:
-    'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8083',
-  ACCOUNT_API: 'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8084',
-  // USER_API: 'http://localhost:8081',
-  // TRANSACTION_API: 'http://localhost:8082',
-  // DASHBOARD_API: 'http://localhost:8083',
-  // ACCOUNT_API: 'http://localhost:8084',
+  // USER_API: 'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8081',
+  // TRANSACTION_API:
+  //   'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8082',
+  // DASHBOARD_API:
+  //   'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8083',
+  // ACCOUNT_API: 'http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com:8084',
+  USER_API: 'http://localhost:8081',
+  TRANSACTION_API: 'http://localhost:8082',
+  DASHBOARD_API: 'http://localhost:8083',
+  ACCOUNT_API: 'http://localhost:8084',
 };
 
 /*
