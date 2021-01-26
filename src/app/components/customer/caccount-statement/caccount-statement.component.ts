0bb02f0d2778b81a16208b6b6307b51157172c23
Himanshu Koshti <Himfeedback74@gmail.com>
2021-01-26 18:40:21 +0530

    Customer table Fixed

--- src/app/components/customer/caccount-statement/caccount-statement.component.ts
+++ src/app/components/customer/caccount-statement/caccount-statement.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
 import { MainService } from 'src/app/services/main.service';
+import { TransactionRecord } from 'src/app/classes/transactionrecord';
+import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-caccount-statement',
   templateUrl: './caccount-statement.component.html',
@@ -10,15 +12,44 @@ import { MainService } from 'src/app/services/main.service';
 export class CaccountStatementComponent implements OnInit {
   Transactions;
   temp: any;
+  ELEMENT_DATA: TransactionRecord[];
+  displayedColumns: string[] = [
+    'transactionId',
+    'senderAccountNumber',
+    'receiverAccountNumber',
+    'amount',
+    'transactionStatus',
+    'balance',
+    'transactionType',
+  ];
+  dataSource = new MatTableDataSource<TransactionRecord>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(
-    private _service: MainService
-  ) {}
+  public getData() {
+    this._service
+      .getTransactionStatement()
+      .subscribe(
+        (report) => (
+          (this.dataSource.data = report as TransactionRecord[]),
+          (this.dataSource.paginator = this.paginator)
+        )
+      );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  constructor(private _service: MainService) {}
 
   ngOnInit(): void {
     this._service
       .getTransactionStatement()
       .subscribe((data) => (this.Transactions = data));
     this.temp = this.Transactions;
+
+    this.getData();
+    this.dataSource.paginator = this.paginator;
   }
 }
--- src/app/components/sample/sample.component.ts
+++ src/app/components/sample/sample.component.ts
@@ -1,30 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MainService } from 'src/app/services/main.service';
+import { TransactionRecord } from 'src/app/classes/transactionrecord';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.css']
+  styleUrls: ['./sample.component.css'],
 })
 export class SampleComponent implements OnInit {
+  ELEMENT_DATA: TransactionRecord[];
+  displayedColumns: string[] = [
+    'transactionId',
+    'senderAccountNumber',
+    'receiverAccountNumber',
+    'amount',
+    'transactionStatus',
+    'balance',
+    'transactionType',
+  ];
+  dataSource = new MatTableDataSource<TransactionRecord>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private router:Router) { }
+  public getData() {
+    this._service
+      .getTransactionStatement()
+      .subscribe(
+        (report) => (
+          (this.dataSource.data = report as TransactionRecord[]),
+          (this.dataSource.paginator = this.paginator)
+        )
+      );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  constructor(private router: Router, private _service: MainService) {}
   public isUserLoggedIn;
+  Transactions;
 
   ngOnInit(): void {
-    if(sessionStorage.getItem("Log"))
-    {
+    if (sessionStorage.getItem('Log')) {
       this.isUserLoggedIn = true;
-    }else{
+    } else {
       this.isUserLoggedIn = false;
     }
+    this.getData();
+    this.dataSource.paginator = this.paginator;
   }
-  onLogout(){    
-    if(sessionStorage.getItem("Log") !== null){
-      sessionStorage.removeItem("Log")
-      this.router.navigateByUrl('home')
-      console.log("SuccessFull Logout")
+  onLogout() {
+    if (sessionStorage.getItem('Log') !== null) {
+      sessionStorage.removeItem('Log');
+      this.router.navigateByUrl('home');
+      console.log('SuccessFull Logout');
     }
   }
-
 }
