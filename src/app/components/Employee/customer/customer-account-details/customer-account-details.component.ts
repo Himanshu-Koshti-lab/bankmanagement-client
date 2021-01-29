9a7b04cfd18e75e75844e74acfe716721597471e
rkrohitkakade <kakade2506@gmail.com>
2021-01-29 12:34:09 +0530

    Employee Table Updated

--- src/app/components/Employee/customer/customer-account-details/customer-account-details.component.ts
+++ src/app/components/Employee/customer/customer-account-details/customer-account-details.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import {AccountDetails} from 'src/app/classes/account-details';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-customer-account-details',
@@ -12,11 +15,43 @@ export class CustomerAccountDetailsComponent implements OnInit {
   AllCustomers: any;
   temp:any;
 
+  ELEMENT_DATA: AccountDetails[];
+
   constructor(private _service:MainService,private _http:HttpClient, private spinner: NgxSpinnerService) { }
+
+  displayedColumns: string[] = [
+    'accountNumber',
+    'firstName',
+    'balance',
+    'accounttype',
+    'accountregstatus'
+  ];
+
+  dataSource = new MatTableDataSource<AccountDetails>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  public getData() {
+    this._service
+      .getAccountDetails()
+      .subscribe(
+        (report) => (
+          (this.dataSource.data = report as AccountDetails[]),
+          (this.dataSource.paginator = this.paginator)
+        )
+      );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   ngOnInit(): void {
     this._service.getAccountDetails().subscribe((data) => this.AllCustomers = data);
     this.temp = this.AllCustomers;
+    this.getData();
+    this.dataSource.paginator = this.paginator;
     this.spinner.show();
     setTimeout(() => {
       /** spinner ends after 5 seconds */
--- src/app/components/Employee/customer/customer-account-statement/customer-account-statement.component.ts
+++ src/app/components/Employee/customer/customer-account-statement/customer-account-statement.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import {AccountStatement} from 'src/app/classes/account-statement';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-customer-account-statement',
@@ -11,12 +14,43 @@ export class CustomerAccountStatementComponent implements OnInit {
 
   Transactions;
   temp: any;
+  ELEMENT_DATA: AccountStatement[];
+
+  displayedColumns: string[] = [
+    'transactionId',
+    'senderAccountNumber',
+    'receiverAccountNumber',
+    'amount',
+    'transactionStatus',
+    'transactionType',
+  ];
+
+  dataSource = new MatTableDataSource<AccountStatement>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private _service: MainService, private spinner: NgxSpinnerService) { }
+
+  public getData() {
+    this._service
+      .getTransactionCustomersStatement()
+      .subscribe(
+        (report) => (
+          (this.dataSource.data = report as AccountStatement[]),
+          (this.dataSource.paginator = this.paginator)
+        )
+      );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   ngOnInit(): void {
     this._service.getTransactionCustomersStatement().subscribe((data) => (this.Transactions = data));
     this.temp = this.Transactions;
+    this.getData();
+    this.dataSource.paginator = this.paginator;
     this.spinner.show();
     setTimeout(() => {
       /** spinner ends after 5 seconds */
